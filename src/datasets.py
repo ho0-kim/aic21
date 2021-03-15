f2d435d542cadca3061f1552eec0fd4e37aa6748
Hoyoung Kim <thesims89g@gmail.com>
2021-03-15 15:38:34 +0900

    first commit

---
+++ src/datasets.py
@@ -0,0 +1,255 @@
+import json
+import os
+import random
+
+import cv2
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pack_sequence
+
+
+class CityFlowNLDataset(Dataset):
+    def __init__(self, data_cfg):
+        """
+        Dataset for training.
+        :param data_cfg: CfgNode for CityFlow NL.
+        """
+        self.data_cfg = data_cfg
+        with open(self.data_cfg["train_json"]) as f:
+            tracks = json.load(f)
+        self.list_of_uuids = list(tracks.keys())
+        self.list_of_tracks = list(tracks.values())
+        # self.list_of_crops = list()
+        # for track in self.list_of_tracks:
+        #     for frame_idx, frame in enumerate(track["frames"]):
+        #         frame_path = os.path.join(self.data_cfg["cityflow_path"], frame)
+        #         nl_idx = int(random.uniform(0, 3))
+        #         nl = track["nl"][nl_idx]
+        #         box = track["boxes"][frame_idx]
+        #         crop = {"frame": frame_path, "nl": nl, "box": box}
+        #         self.list_of_crops.append(crop)
+        # self._logger = get_logger()
+
+    def __len__(self):
+        # return len(self.list_of_crops)
+        return len(self.list_of_uuids)
+
+    def __getitem__(self, index):
+        """
+        Get pairs of NL and cropped frame.
+        """
+        if random.uniform(0, 1) > self.data_cfg["positive_threshold"]:
+            label = 1
+        else:
+            label = 0
+
+        track = self.list_of_tracks[index]
+
+        frames = []
+        crops = []
+        histograms = []
+        segments = []
+        boxes = []
+        positions = []
+        for i, frame_path in enumerate(track["frames"]):
+            frame = cv2.imread(frame_path)
+            box = track["boxes"][i]
+            crop = frame[box[1]:box[1] + box[3], box[0]: box[0] + box[2], :]
+            crop = cv2.resize(crop, dsize=tuple(self.data_cfg["crop_size"]))
+            hist = [cv2.calcHist([crop],[0],None,[256],[0,256]), # B
+                    cv2.calcHist([crop],[1],None,[256],[0,256]), # G
+                    cv2.calcHist([crop],[2],None,[256],[0,256])] # R
+
+            img_file = os.path.basename(frame_path)
+            img_name = img_file.split(".")[0]
+            seg_path = os.path.dirname(os.path.dirname(frame_path))
+            seg_path = os.path.join(seg_path, "seg")
+            seg_path = os.path.join(seg_path, img_name+"_prediction.png")
+            segmented = cv2.imread(seg_path)
+
+            frame = cv2.resize(frame, dsize=tuple(self.data_cfg["frame_size"]))
+            segmented = cv2.resize(segmented, dsize=tuple(self.data_cfg["frame_size"]))
+            
+            frames.append(torch.from_numpy(frame).cuda())
+            crops.append(torch.from_numpy(crop).cuda())
+            histograms.append(torch.FloatTensor(hist).cuda())
+            positions.append(torch.FloatTensor([box[1], box[0]]).cuda())
+            segments.append(torch.from_numpy(segmented).cuda())
+            boxes.append(torch.FloatTensor(box).cuda())
+
+        dp = {}
+        dp["frames"] = frames
+        dp["crops"] = crops
+        dp["histograms" ] = histograms
+        dp["segments"] = segments
+        dp["boxes"] = boxes
+        dp["positions"] = positions
+        dp["label"] = torch.Tensor([label]).to(dtype=torch.float32).cuda()
+        dp["nl"] = random.sample(track["nl"], 3)
+
+        return dp
+
+    def collate_fn(self, batch):
+        # Zero pads
+        lengths = [ len(t["frames"]) for t in batch ]
+        max_len = max(lengths)
+
+        for i, b in enumerate(batch):
+            for _ in range(max_len - lengths[i]):
+                b["frames"].append(torch.zeros_like(b["frames"][0]).cuda())
+                b["crops"].append(torch.zeros_like(b["crops"][0]).cuda())
+                b["histograms"].append(torch.zeros_like(b["histograms"][0]).cuda())
+                b["segments"].append(torch.zeros_like(b["segments"][0]).cuda())
+                b["boxes"].append(torch.zeros_like(b["boxes"][0]).cuda())
+                b["positions"].append(torch.zeros_like(b["positions"][0]).cuda())
+
+            b["sequence_len"] = torch.Tensor([lengths[i]]).cuda()
+
+            b["frames"] = torch.stack(b["frames"]).cuda()
+            b["crops"] = torch.stack(b["crops"]).cuda()
+            b["histograms"] = torch.stack(b["histograms"]).cuda()
+            b["segments"] = torch.stack(b["segments"]).cuda()
+            b["boxes"] = torch.stack(b["boxes"]).cuda()
+            b["positions"] = torch.stack(b["positions"]).cuda()
+
+        ret = {}
+        ret["frames"] = torch.stack([ b["frames"] for b in batch ]).cuda()
+        ret["crops"] = torch.stack([ b["crops"] for b in batch ]).cuda()
+        ret["histograms"] = torch.stack([ b["histograms"] for b in batch ]).cuda()
+        ret["segments"] = torch.stack([ b["segments"] for b in batch ]).cuda()
+        ret["boxes"] = torch.stack([ b["boxes"] for b in batch ]).cuda()
+        ret["positions"] = torch.stack([ b["positions"] for b in batch ]).cuda()
+        ret["label"] = torch.stack([ b["label"] for b in batch ]).cuda()
+        ret["nl"] = [ b["nl"] for b in batch ]
+
+        return ret
+
+
+
+
+class CityFlowNLInferenceDataset(Dataset):
+    def __init__(self, data_cfg):
+        """Dataset for evaluation. Loading tracks instead of frames."""
+        self.data_cfg = data_cfg
+        with open(self.data_cfg.EVAL_TRACKS_JSON_PATH) as f:
+            tracks = json.load(f)
+        self.list_of_uuids = list(tracks.keys())
+        self.list_of_tracks = list(tracks.values())
+        # self._logger = get_logger()
+
+    def __len__(self):
+        return len(self.list_of_uuids)
+
+    def __getitem__(self, index):
+        """
+        :return: a dictionary for each track:
+        id: uuid for the track
+        frames, boxes, nl are untouched from the input json file.
+        crops: A Tensor of cropped images from the track of shape
+            [length, 3, crop_w, crop_h].
+        """
+        dp = {"id": self.list_of_uuids[index]}
+        dp.update(self.list_of_tracks[index])
+        cropped_frames = []
+        for frame_idx, frame_path in enumerate(dp["frames"]):
+            frame_path = os.path.join(self.data_cfg.CITYFLOW_PATH, frame_path)
+            if not os.path.isfile(frame_path):
+                continue
+            frame = cv2.imread(frame_path)
+            box = dp["boxes"][frame_idx]
+            crop = frame[box[1]:box[1] + box[3], box[0]: box[0] + box[2], :]
+            crop = cv2.resize(crop, dsize=self.data_cfg.CROP_SIZE)
+            crop = torch.from_numpy(crop).permute([2, 0, 1]).to(
+                dtype=torch.float32)
+            cropped_frames.append(crop)
+        dp["crops"] = torch.stack(cropped_frames, dim=0)
+        return dp
+
+# def collate_fn(batch):
+#     # if isinstance(batch, BBoxList):
+#         # return batch
+#     return batch
+#     # return default_collate(batch)
+
+if __name__ == '__main__':
+    config_json = "src/config.json"
+
+    with open(config_json) as f:
+        conf = json.load(f)
+
+    # print(conf)
+    # print(conf["data"])
+    # print(conf["data"]["train_json"])
+
+    # data_conf = conf["data"]
+    # print(data_conf["train_json"])
+
+
+    import torch.distributed as dist
+    from torch.utils.data import DataLoader, DistributedSampler, SequentialSampler, RandomSampler
+    from torch.nn.parallel import DistributedDataParallel
+
+    # rank = 0
+    # dist_rank = 0
+    # WORLD_SIZE = 1
+    # INIT_METHOD = "file://../baseline/shared_file.txt" #"tcp://%s:%d" % ("127.0.0.1", 12000)
+    # dist.init_process_group(backend="gloo", rank=dist_rank,
+    #                         world_size=WORLD_SIZE,
+    #                         init_method=INIT_METHOD)
+
+    cfg = conf
+
+    dataset = CityFlowNLDataset(cfg["data"])
+
+    # train_sampler = DistributedSampler(dataset)
+    # train_sampler = SequentialSampler(dataset)
+    train_sampler = RandomSampler(dataset)
+    dataloader = DataLoader(dataset, batch_size=cfg["train"]["batch_size"], #cfg.TRAIN.BATCH_SIZE,
+                            num_workers=cfg["train"]["num_workers"], #cfg.TRAIN.NUM_WORKERS,
+                            sampler=train_sampler,
+                            collate_fn=dataset.collate_fn)
+    print('dataloader set')
+
+    i = 0
+    for minibatch in dataloader:
+        i += 1
+        
+        print(type(minibatch))
+        print(len(minibatch))
+        tracks = minibatch
+        print(type(tracks[0]["frames"]))
+        print(tracks[0]["frames"][-1].shape)
+
+        # print(track["frames"][0].shape)
+        # print(track["crops"][0].shape)
+        # print(track["histograms"][0].shape)
+        # print(track["segments"][0].shape)
+        # print(track["boxes"][0].shape)
+        # print(track["positions"][0].shape)
+        # print(track["label"][0].shape)
+
+        # print(len(track["frames"]))
+        # print(len(track["crops"]))
+        # print(len(track["histograms"]))
+        # print(len(track["segments"]))
+        # print(len(track["boxes"]))
+        # print(len(track["positions"]))
+        # print(len(track["label"]))
+
+        # print(track["nl"])
+
+        print(f'iter: {i}')
+
+        if i == 1:
+            break
+        # dp["frames"] = frames
+        # dp["crops"] = crops
+        # dp["histograms" ] = histograms
+        # dp["segments"] = segments
+        # dp["boxes"] = track["boxes"]
+        # dp["positions"] = positions
+        # dp["label"] = torch.Tensor([label]).to(dtype=torch.float32)
+
+        #OSError: [Errno 24] Too many open files
+        #solution: ulimit -n 10240
---
+++ src/model.py
@@ -0,0 +1,85 @@
+import collections
+import itertools
+import logging
+import re
+import types
+
+import bert as VidBertModel
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from transformers.modeling_bert import BertModel as TxtBertModel
+from transformers import BertTokenizer, BertModel
+
+class ReduceDim(nn.Module):
+
+    def __init__(self, input_dimension, output_dimension):
+        super(ReduceDim, self).__init__()
+        self.fc = nn.Linear(input_dimension, output_dimension)
+
+    def forward(self, x):
+        x = self.fc(x) 
+        x = F.normalize(x, dim=-1)
+        return x
+
+class CEModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+
+        self.txt_tokenizer = BertTokenizer.from_pretrained(self.cfg["bert_pretrained_model"],
+                                                            do_lower_case=True)
+        self.txt_bert = BertModel.from_pretrained(self.cfg["bert_pretrained_model"])
+        self.lang_fc = torch.nn.Linear(768, self.cfg["embedding_size"])
+
+    def forward(self, input):
+        print(type(input))
+        print(len(input))
+
+        print(input["nl"])
+        print(input["frames"].shape)
+
+        txt_embd_list = []
+        for minibatch in input["nl"]:
+            token = self.txt_tokenizer(minibatch,
+                                        padding=True,
+                                        truncation=True,
+                                        max_length=64,
+                                        return_tensors="pt")
+            txt_embd = self.txt_bert(**token).last_hidden_state
+            txt_embd = torch.mean(txt_embd, dim=1)
+            txt_embd = self.lang_fc(txt_embd)
+            txt_embd_list.append(txt_embd)
+        txt_embds = torch.stack(txt_embd_list).cuda()
+
+
+if __name__ == '__main__':
+    print(f'running script {__file__}')
+
+    import json
+    from datasets import CityFlowNLDataset
+    from torch.utils.data import DataLoader, RandomSampler
+
+    config_json = "src/config.json"
+
+    with open(config_json) as f:
+        cfg = json.load(f)
+
+    dataset = CityFlowNLDataset(cfg["data"])
+    train_sampler = RandomSampler(dataset)
+    dataloader = DataLoader(dataset, batch_size=cfg["train"]["batch_size"], #cfg.TRAIN.BATCH_SIZE,
+                            num_workers=cfg["train"]["num_workers"], #cfg.TRAIN.NUM_WORKERS,
+                            sampler=train_sampler,
+                            collate_fn=dataset.collate_fn)
+
+    model = CEModel(cfg=cfg["model"])
+
+    i = 0
+    for batch in dataloader:
+        i += 1
+
+        model(batch)
+
+        if i == 1:
+            break
